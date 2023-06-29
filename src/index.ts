d538f3bd9be3b977f9b87ba5e0d810424ef85bc6
saturdaythrowaway <saturdaythrowaway@proton.me>
2023-06-29 15:03:56 -0400

    updated release info

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const { core, console, file, mpv, utils, http, event, overlay, preferences } = i
 
 if (!file.exists("@data/tcode-player")) {
   console.log("Downloading tcode-player...");
-  http.download("https://github.com/saturdaythrowaway/iina-tcode", "@data/tcode-player")
+  http.download("https://github.com/saturdaythrowaway/iina-tcode/releases/latest/download/tcode-player", "@data/tcode-player")
 } else {
   utils.exec("@data/tcode-player", ["--logfile", "/tmp/tcode-player.log", "listen", "&"]);
 }
